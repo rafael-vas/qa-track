52ac0d685c6b89fe8ab770191ebfff2533e1640b
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2026-04-16 16:10:23 -0300

    criar classe Bug com passos de reprodução e ambiente

---
+++ src/sistema/model/Bug.java
@@ -0,0 +1,59 @@
+package sistema.model;
+
+public class Bug extends Ticket {
+    private String passosParaReproduzir;
+    private String ambiente;
+
+    public Bug(
+        int id,
+        String titulo,
+        String descricao,
+        Usuario criador,
+        Prioridade prioridade,
+        String passosParaReproduzir
+    ) {
+        this(id, titulo, descricao, criador, prioridade, passosParaReproduzir, "Nao informado");
+    }
+
+    public Bug(
+        int id,
+        String titulo,
+        String descricao,
+        Usuario criador,
+        Prioridade prioridade,
+        String passosParaReproduzir,
+        String ambiente
+    ) {
+        super(id, titulo, descricao, criador, prioridade);
+
+        if (passosParaReproduzir == null || passosParaReproduzir.isBlank()) {
+            this.passosParaReproduzir = "Nao informado";
+        } else {
+            this.passosParaReproduzir = passosParaReproduzir;
+        }
+
+        if (ambiente == null || ambiente.isBlank()) {
+            this.ambiente = "Nao informado";
+        } else {
+            this.ambiente = ambiente;
+        }
+    }
+
+    public String getPassosParaReproduzir() {
+        return passosParaReproduzir;
+    }
+
+    public String getAmbiente() {
+        return ambiente;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Bug";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Ambiente: " + ambiente;
+    }
+}
