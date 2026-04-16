52ceba1ecc7dbaec0ebd942ea4a8cc9b74d5d6fc
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2026-04-16 16:12:51 -0300

    adicionar construtor, getters, setNome, getTipo e toString em Usuario

--- src/sistema/model/Usuario.java
+++ src/sistema/model/Usuario.java
@@ -3,4 +3,36 @@ package sistema.model;
 public abstract class Usuario {
     private int id;
     private String nome;
+
+    public Usuario(int id, String nome) {
+        this.id = id;
+
+        if (nome == null || nome.isBlank()) {
+            this.nome = "Usuario";
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.isBlank()) {
+            return;
+        }
+        this.nome = nome;
+    }
+
+    public abstract String getTipo();
+
+    @Override
+    public String toString() {
+        return nome + " (" + getTipo() + ")";
+    }
 }
